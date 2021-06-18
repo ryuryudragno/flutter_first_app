0f8b42d492b1be953fb6319e16b1dd96ebf0ca4b
chelsea <ryuno138@gmail.com>
2021-06-19 02:06:18 +0900

    tip calculator app

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -38,9 +38,20 @@ class _BillSplitterState extends State<BillSplitter> {
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Text("Total per Person"),
+                    Text("Total per Person",style: TextStyle(
+                      fontWeight: FontWeight.normal,
+                      fontSize: 15.0,
+                      color: _purple,
+                    ),),
 
-                    Text("\$123"),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text("\$ ${calculateTotalPerPerson(_tipPercentage, _billAmount, _personCounter)}",style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 34.9,
+                        color: _purple,
+                      ),),
+                    ),
                   ],
                 ),
               ),
@@ -73,6 +84,118 @@ class _BillSplitterState extends State<BillSplitter> {
                         _billAmount = 0.0;
                       }
                     },
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text("Split",style: TextStyle(
+                        color: Colors.grey.shade700
+                      ),),
+                      Row(
+                        children: [
+                          InkWell(
+                            onTap: (){
+                              setState(() {
+                                if(_personCounter > 1){
+                                  _personCounter--;
+                                }else{
+
+                                }
+                              });
+                            },
+                            child: Container(
+                              width: 40,
+                              height: 40,
+                              margin: EdgeInsets.all(10),
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(7),
+                                color: _purple.withOpacity(0.1),
+                              ),
+                              child: Center(
+                                child: Text(
+                                  "-",style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 17.0
+                                ),
+                                ),
+                              ),
+                            ),
+
+                          ),
+                          Text("$_personCounter",style: TextStyle(
+                            color: _purple,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 17.0,
+                          ),),
+                          InkWell(
+                            onTap: (){
+                              setState(() {
+                                _personCounter++;
+                              });
+                            },
+                            child: Container(
+                              width: 40,
+                              height: 40,
+                              margin: EdgeInsets.all(10),
+                              decoration: BoxDecoration(
+                                color: _purple.withOpacity(0.1),
+                                borderRadius: BorderRadius.circular(7.0)
+                              ),
+                              child: Center(
+                                child: Text(
+                                  "+",style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 17.0
+                                ),
+                                ),
+                              ),
+                            ),
+                          ),
+
+                        ],
+                      ),
+
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text("Tip",style: TextStyle(
+                          color: Colors.grey.shade700
+                      ),),
+                      Padding(
+                        padding: const EdgeInsets.all(18.0),
+                        child: Text(" \$ ${calculateTotalTip(_billAmount,
+                            _tipPercentage, _personCounter)}",style: TextStyle(
+                          color: _purple,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 17.0,
+                        ),),
+                      )
+                    ],
+                  ),
+                  //Slider
+                  Column(
+                    children: [
+                      Text("$_tipPercentage %",style: TextStyle(
+                        color: _purple,
+                        fontSize: 17.0,
+                        fontWeight: FontWeight.bold,
+                      ),),
+                      Slider(
+                          min:0,
+                          max:100,
+                          activeColor: _purple,
+                          inactiveColor: Colors.grey,
+                          divisions: 10,
+                          value: _tipPercentage.toDouble(),
+                          onChanged: (double newValue){
+                            setState(() {
+                              _tipPercentage = newValue.round();
+                            });
+                          }
+                      )
+                    ],
                   )
                 ],
               ),
@@ -81,6 +204,22 @@ class _BillSplitterState extends State<BillSplitter> {
         ),
       ),
     );
+  }
+  calculateTotalPerPerson(int tipPercentage, double billAmount, int splitby){
+    var totalPerPerson = (calculateTotalTip(billAmount, tipPercentage, splitby) + billAmount)/splitby;
+
+    return totalPerPerson.toStringAsFixed(2);
+  }
+
+  calculateTotalTip(double billAmount, int tipPercentage, int splitby){
+    double totalTip = 0.0;
+
+    if(billAmount < 0 || billAmount.toString().isEmpty || billAmount == null){
+
+    }else{
+      totalTip = (billAmount *tipPercentage)/100;
+    }
+    return totalTip;
   }
 }
 
